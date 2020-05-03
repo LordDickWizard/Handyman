98ea4219487b0ce62ba0e840ae50ede607ea2b4a
LordDickWizard <LordDickWizard@gmail.com>
2020-05-03 21:03:30 +0100

    Added movement and collision

---
+++ Handyman/Assets/Scripts/cameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollower : MonoBehaviour
+{
+
+    public Transform cameraParent;
+    public Vector3 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraParent.position + offSet;
+    }
+}
