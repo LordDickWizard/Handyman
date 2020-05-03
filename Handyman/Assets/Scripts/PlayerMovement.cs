d3e75efc46c3374f1a6867340daa7c796c782f23
LordDickWizard <LordDickWizard@gmail.com>
2020-05-03 14:57:22 +0100

    adding objects to the scene
    Messing with physics

---
+++ Handyman/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        rb.AddForce(0, 600, 500);
+    }
+
+    // Update is called once per frame for physics
+    void FixedUpdate()
+    {
+        //rb.AddForce(0, 0, 800 * Time.deltaTime);
+    }
+}
---
+++ Handyman/Assets/Scripts/WorldObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldObjectController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        //rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame for physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 800 * Time.deltaTime);
+    }
+}
